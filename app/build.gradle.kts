2df5480b8398debdd089106ac613a2be1f83de31
altunoymak <yusuf_altunoymak@hotmail.com>
2024-06-13 16:13:13 +0300

    Manifest and Version Catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin)
+    //Hilt
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+    //Navigation
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -33,6 +39,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +51,43 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Hilt
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+    //Navigation
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+    //Retrofit
+    implementation (libs.gson)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    //Glide
+    implementation (libs.glide)
+    //Logging Interceptor
+    implementation (libs.okhttp)
+    implementation (libs.logging.interceptor)
+    //Room
+    implementation (libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    annotationProcessor (libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
+    //ViewModel
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.runtime.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.lifecycle.common.java8)
+    implementation (libs.androidx.lifecycle.viewmodel.savedstate)
+    implementation (libs.android.maps.utils)
+    implementation (libs.androidx.fragment.ktx)
+    implementation (libs.androidx.activity.ktx)
+
+    //Lottie
+    implementation(libs.lottie)
+}
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin) apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+    }
 }
