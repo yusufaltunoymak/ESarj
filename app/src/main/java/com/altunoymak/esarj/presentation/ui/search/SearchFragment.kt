7a22a9809bac362164032b960431b8a398b44ab8
altunoymak <yusuf_altunoymak@hotmail.com>
2024-06-21 16:27:25 +0300

    Unused Import Deleting and String Resource

--- app/src/main/java/com/altunoymak/esarj/presentation/ui/search/SearchFragment.kt
+++ app/src/main/java/com/altunoymak/esarj/presentation/ui/search/SearchFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
